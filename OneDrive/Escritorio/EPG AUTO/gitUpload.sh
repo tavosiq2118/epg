6e7e280c09bf9d9f72b2136f60e58f60f184d858
tavosiq2118 <tavosiq2118@gmail.com>
2022-07-03 18:01:32 -0600

    commit de la guia

---
+++ OneDrive/Escritorio/EPG AUTO/gitUpload.sh
@@ -0,0 +1,5 @@
+cd C:/Users\gusta/OneDrive/Escritorio/EPG AUTO/GUIA EPG
+git add .
+git commit -m 'commit de la guia'
+git remote add origin https://github.com/tavosiq2118/epg
+git push --force origin master
---
+++ OneDrive/Escritorio/EPG AUTO/move.bat
@@ -0,0 +1,1 @@
+MOVE "C:\Users\gusta\AppData\Local\WebGrab+Plus\guide.xml" "C:\Users\gusta\OneDrive\Escritorio\EPG AUTO\GUIA EPG"
